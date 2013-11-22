8df5aafb2cd4cd82ff23b21a837ebefb21e850b1
Anson Hoyt <ahoyt@tiu11.org>
2013-11-22 16:18:33 -0500

    Setup ruby and gemset with panache and color. Put some dashes in when app_name is CamelCased.

--- template.rb
+++ template.rb
@@ -1,3 +1,14 @@
+# Check prerequisite gems
+%w{colored}.each do |gemname|
+  if Gem::Specification.find_all_by_name(gemname).empty?
+    run "gem install #{gemname}"
+    Gem.refresh
+    Gem.activate(gemname)
+  end
+end
+
+require 'colored'
+
 # Download template files
 git archive: "--remote=git@bitbucket.org:tiu/rails-application-template.git --format=tar --verbose master:rails | (tar xf -)"
 
@@ -19,7 +30,6 @@ route "root to: 'exception#show'"
 route "match '/404' => 'exception#show'"
 
 # Process Templates
-template "#{destination_root}/.ruby-gemset.tt"
 template "#{destination_root}/config/locales/en.yml.tt"
 template "#{destination_root}/config/initializers/exception_notification.rb.tt"
 template "#{destination_root}/lib/custom_public_exceptions.rb.tt"
@@ -32,17 +42,29 @@ remove_file 'app/assets/images/rails.png'
 remove_file 'public/index.html'
 
 #
+# Create gemset
+#
+puts "Setting up RVM gemset and installing bundled gems (may take a while)".cyan.bold.bold
+current_ruby = `rvm current`.strip
+desired_ruby = ask("Which RVM Ruby would you like to use? [#{current_ruby}]".cyan.bold)
+desired_ruby = current_ruby if desired_ruby.blank?
+gemset_name = app_name.titleize.parameterize
+run "rvm install #{desired_ruby}"
+run "rvm #{current_ruby} do rvm --ruby-version --create use #{desired_ruby}@#{gemset_name}"
+run "rvm #{desired_ruby}@#{gemset_name} do bundle install"
+
+#
 # Generate and Setup
 #
-run 'bundle install'
+
 generate 'bootstrap:install less'
 generate 'rspec:install'
 
-if yes?("Create Users? (yN)")
+if yes?("Create Users? [yN]".cyan.bold)
   generate 'authlogic:install'
 end
 
-if yes?("Initialize the Bitbucket Git repository? (yN)")
+if yes?("Initialize the Bitbucket Git repository? [yN]".cyan.bold)
   require 'json'
 
   # Create Bitbucket Repository
@@ -51,12 +73,12 @@ if yes?("Initialize the Bitbucket Git repository? (yN)")
     scm: 'git',
     is_private: true,
     forking_policy: 'allow_forks',
-    name: app_name,
+    name: app_name.titleize,
     language: 'ruby'
   }
-  repo_slug = @app_name.parameterize
+  repo_slug = @app_name.titleize.parameterize
   owner = 'tiu'
-  credentials = ask("What are your TIU Bitbucket credentials? (username:password)").strip # TODO: can we drop this prompt?
+  credentials = ask("What are your TIU Bitbucket credentials? (username:password)".cyan.bold).strip # TODO: can we drop this prompt?
   # --user username:password
   # --pubkey ~/.ssh/id_rsa.pub # TODO: can we make this work???
   run "curl --request POST --user #{credentials} --header 'Content-Type: application/json' https://bitbucket.org/api/2.0/repositories/#{owner}/#{repo_slug} --data '#{data.to_json}'"
@@ -64,10 +86,10 @@ if yes?("Initialize the Bitbucket Git repository? (yN)")
   # Add code to the repository
   git :init
   git add: '--all .', commit: "-m 'Applied Rails Application Template'"
-  git remote: "add origin ssh://git@bitbucket.org/#{owner}/#{@app_name.parameterize}.git"
+  git remote: "add origin ssh://git@bitbucket.org/#{owner}/#{@app_name.titleize.parameterize}.git"
   git push: "-u origin --all"
 
-  if yes?("Deploy? (yN)")
+  if yes?("Deploy? [yN]".cyan.bold)
     # Add deploy keys to the repository
     key = `ssh dev.tiu11.org "cat ~/.ssh/id_rsa.pub"`
     label = key.split(' ')[2]
