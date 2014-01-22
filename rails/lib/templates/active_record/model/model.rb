aaffb303e4a6a6847051a775e7001dfdae9543b7
Anson Hoyt <ahoyt@tiu11.org>
2014-01-22 09:33:54 -0500

    Scaffold model

---
+++ rails/lib/templates/active_record/model/model.rb
@@ -0,0 +1,45 @@
+<% module_namespacing do -%>
+class <%= class_name %> < <%= parent_class_name.classify %>
+  # extend FriendlyId
+  # friendly_id :name, use: :slugged
+
+  #
+  # Associations
+  #
+<% attributes.select(&:reference?).each do |attribute| -%>
+  belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
+<% end -%>
+<% if attributes.any?(&:password_digest?) -%>
+  has_secure_password
+<% end -%>
+
+  #
+  # Scopes
+  #
+
+  # scope :sorted, ->{ order('name ASC') }
+
+  #
+  # Callbacks
+  #
+
+  #
+  # Validations
+  #
+
+  # validates :name,
+  #           :presence => true
+
+  #
+  # Class Methods
+  #
+
+  #
+  # Methods
+  #
+
+  # def to_s
+  #   "#{name}"
+  # end
+end
+<% end -%>
