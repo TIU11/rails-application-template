794496f04c445de94ac8842342c6d5c7b092f1a8
Anson Hoyt <ahoyt@tiu11.org>
2017-03-17 13:48:46 -0400

    add LoggingUtility.log method that doesn’t send a notification

--- rails/lib/logging_utility.rb
+++ rails/lib/logging_utility.rb
@@ -2,20 +2,27 @@ module LoggingUtility
 
   class << self
 
+    # Logs exception and backtrace to aid troubleshooting.
     # Sends exception to exception_notifier along with context information.
-    # Logs the backtrace to aid troubleshooting.
     #
     # Usage: notify(RuntimeError, message: 'We have a problem')
     def notify(exception, message: nil)
-      backtrace = Rails.backtrace_cleaner.filter(exception.backtrace)
       data = { application: I18n.t('app.title'), environment: Rails.env, message: message }
 
-      Rails.logger.error <<~MESSAGE.strip
-        #{exception.class} - #{exception.message}:
-          #{backtrace.join("#{$/}  ")}
-      MESSAGE
+      log(exception, message: message)
 
       ExceptionNotifier.notify_exception(exception, env: Rails.env, data: data)
+    end
+
+    # Logs exception and backtrace to aid troubleshooting.
+    def log(exception, message: nil)
+      backtrace = Rails.backtrace_cleaner.filter(exception.backtrace) if exception.backtrace
+
+      Rails.logger.error <<~MESSAGE.strip
+        #{message}
+        #{exception.class} - #{exception.message}:
+          #{backtrace&.join("#{$/}  ")}
+      MESSAGE
     end
 
   end
