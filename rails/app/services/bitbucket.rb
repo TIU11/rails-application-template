3e17dcb1432d672c834350aacf569956a08c4afd
Anson Hoyt <ahoyt@tiu11.org>
2017-09-21 09:48:40 -0400

    Simplify implementation a bit.

--- rails/app/services/bitbucket.rb
+++ rails/app/services/bitbucket.rb
@@ -6,45 +6,25 @@ class Bitbucket
   def_delegators self, :username, :password, :credentials, :owner, :repo_slug
 
   class << self
+
+    # Test git connection and get current user's username
     def username
-      @username ||= fetch_username
-    end
-
-    def password
-      @password ||= fetch_password
-    end
-
-    def credentials
-      [username, password].join(':')
-    end
-
-    def owner
-      @owner ||= fetch_remote_info[0]
-    end
-
-    def repo_slug
-      @repo_slug ||= fetch_remote_info[1]
-    end
-
-    private
-
-      def fetch_username
-        # Test git connection and get username
-        puts test_result = `ssh -T git@bitbucket.org`
-        username = test_result[/logged in as (\w+)./, 1]
-        unless username
+      @username ||= begin
+        if `ssh -T git@bitbucket.org` =~ /logged in as (\w+)./
+          $1
+        else
           puts <<~MSG.red
             Looks like you're not setup for Bitbucket yet
             To troubleshoot, see https://confluence.atlassian.com/bitbucket/troubleshoot-ssh-issues-271943403.html
           MSG
         end
-
-        username
       end
+    end
 
-      # TODO: looks like prompting for a password can be replaced with newer options
-      # @see (https://developer.atlassian.com/bitbucket/api/2/reference/meta/authentication)
-      def fetch_password
+    # TODO: looks like prompting for a password can be replaced with newer options
+    # @see (https://developer.atlassian.com/bitbucket/api/2/reference/meta/authentication)
+    def password
+      @password ||= begin
         puts "What is your Bitbucket password?".cyan
         password = STDIN.noecho(&:gets).chomp
         raise ArgumentError if password.blank?
@@ -53,6 +33,21 @@ class Bitbucket
         puts 'password cannot be empty'.red
         retry
       end
+    end
+
+    def credentials
+      [username, password].join(':')
+    end
+
+    def owner
+      @owner || fetch_remote_info[0]
+    end
+
+    def repo_slug
+      @repo_slug || fetch_remote_info[1]
+    end
+
+    private
 
       # Read Bitbucket owner and repo_slug from git remote origin.
       # Look for remote origin that look like:
@@ -61,11 +56,8 @@ class Bitbucket
       # origin	git@bitbucket.org:tiu/foo-bar.git (push)
       def fetch_remote_info
         remotes = `git remote -v`
-        match = %r{origin\s+(\w+://)?\w+@bitbucket.org[/:](\w+)/([\w-]+).git}.match remotes
-        @owner = match && match[2]
-        @repo_slug = match && match[3]
-
-        [@owner, @repo_slug]
+        match = %r{origin\s+(?:\w+://)?\w+@bitbucket.org[/:](\w+)/([\w-]+).git}.match remotes
+        @owner, @repo_slug = match&.captures
       end
   end
 
