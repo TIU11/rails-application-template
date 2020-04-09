2e0e785f6c4b3ef7a21971c658f0cc72f3d232d9
Anson Hoyt <ahoyt@tiu11.org>
2020-04-09 12:26:36 -0400

    Refactor I18n for tiu:authlogic:install - more illustrative example for 'helpers' - update component translations for will_paginate and cancancan

--- rails/config/initializers/i18n.rb
+++ rails/config/initializers/i18n.rb
@@ -2,7 +2,7 @@
 
 # Where the I18n library should search for translation files
 # Loads locales in nested subfolders, not just in the default directory
-Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+Rails.application.config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
 
 # Make missing translations fail loudly in development and test.
 # See https://robots.thoughtbot.com/better-tests-through-internationalization
