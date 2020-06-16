e669805ea4e75a57b42b08a989001b7b2818867b
Anson Hoyt <ahoyt@tiu11.org>
2020-06-16 16:52:08 -0400

    Handle nil status

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -28,7 +28,7 @@ module ApplicationHelper
     status = status.status if status.is_a? ApplicationRecord
 
     span_class = CSS_CLASS_FOR_STATUS[status.to_s]
-    tag.span status.titleize, class: "badge #{span_class} #{binding.local_variable_get(:class)}"
+    tag.span status&.titleize, class: "badge #{span_class} #{binding.local_variable_get(:class)}"
   end
 
   # +message+ optional message to override the default
