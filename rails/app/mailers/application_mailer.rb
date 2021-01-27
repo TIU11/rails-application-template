fb1a7e30ba0cc35e1b7e02b06f7862cc66f16fb1
Anson Hoyt <ahoyt@tiu11.org>
2021-01-27 08:59:43 -0500

    Add a default application_mailer.rb.

---
+++ rails/app/mailers/application_mailer.rb
@@ -0,0 +1,19 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: I18n.t('app.sending_email'),
+          reply_to: I18n.t('app.support_email')
+
+  # Include view helpers
+  #
+  # Avoids add_template_helper, which was removed in Rails 6.1
+  # See https://github.com/rails/rails/commit/cb3b37b37975ceb1d38bec9f02305ff5c14ba8e9
+
+  include AnalyticsHelper
+  helper AnalyticsHelper
+
+  include DateRangeHelper # for :date_range method
+  helper DateRangeHelper
+
+  include DateTimeHelper # for :readable_date method
+  helper DateTimeHelper
+
+end
