8bcd79f63629b5826aa459723ebc05e45944a5c2
Anson Hoyt <ahoyt@tiu11.org>
2021-11-23 11:29:41 -0500

    Capistrano config - remove public/system from :linked_dirs since we're not using it - remove 'deploy:update_group' task now that permissions are fixed (setgid) on /export/home - separate :db role for the database server. - set :migration_role to :app so migrations run there

--- rails/config/deploy/dev.rb
+++ rails/config/deploy/dev.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-server 'dev.tiu11.org', user: fetch(:user), roles: %w[web app db], ssh_options: {
+server 'dev.tiu11.org', user: fetch(:user), roles: %w[web app], ssh_options: {
   forward_agent: true,
   auth_methods: %w[publickey]
 }
 
+server 'dev.tiu11.org', user: fetch(:user), roles: %w[db], no_release: true
+
 # Parallelize the installation of gems.
 # Choose a number less or equal than the number of cores your server.
-set :bundle_jobs, 2
+set :bundle_jobs, 4
