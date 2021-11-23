5dbd476ab9cb52c442eb726dc21913780fc6256d
Anson Hoyt <ahoyt@tiu11.org>
2021-11-23 13:43:22 -0500

    Handle passing a Symbol to `status_tag`

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -29,7 +29,7 @@ module ApplicationHelper
     return if status.blank?
 
     span_class = CSS_CLASS_FOR_STATUS[status.to_s]
-    tag.span status.titleize, class: "badge #{span_class} #{binding.local_variable_get(:class)}"
+    tag.span status.to_s.titleize, class: "badge #{span_class} #{binding.local_variable_get(:class)}"
   end
 
   # +message+ optional message to override the default
