7dae0e1369588596050658055c60448ccb595449
Anson Hoyt <ahoyt@tiu11.org>
2021-12-07 08:28:42 -0500

    [Capistrano] Set per-stage :fqdn

--- rails/config/deploy/dev.rb
+++ rails/config/deploy/dev.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+# Fully-qualified Domain Name
+set :fqdn, "#{fetch(:application)}-#{fetch(:stage)}.#{fetch(:top_level_domain)}"
+
 server 'dev.tiu11.org', user: fetch(:user), roles: %w[web app], ssh_options: {
   forward_agent: true,
   auth_methods: %w[publickey]
 }
 
-server 'dev.tiu11.org', user: fetch(:user), roles: %w[db], no_release: true
+server 'db-dev.tiu11.org', user: fetch(:user), roles: %w[db], no_release: true
 
 # Parallelize the installation of gems.
 # Choose a number less or equal than the number of cores your server.
--- rails/config/deploy/production.rb
+++ rails/config/deploy/production.rb
@@ -3,12 +3,15 @@
 # TODO: Specify proxy if needed
 # set :default_env, http_proxy: 'http://172.16.x.x:xxxx'
 
-server 'production.tiu11.org', user: fetch(:user), roles: %w[web app], ssh_options: {
+# Fully-qualified Domain Name
+set :fqdn, "#{fetch(:application)}.#{fetch(:top_level_domain)}"
+
+server 'app-production.tiu11.org', user: fetch(:user), roles: %w[web app], ssh_options: {
   forward_agent: true,
   auth_methods: %w[publickey]
 }
 
-server 'database.tiu11.org', user: fetch(:user), roles: %w[db], no_release: true
+server 'db-production.tiu11.org', user: fetch(:user), roles: %w[db], no_release: true
 
 # Parallelize the installation of gems.
 # Choose a number less or equal than the number of cores your server.
