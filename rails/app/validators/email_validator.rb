9f4fee0517b9cb1ded8240dd885a17d242429b77
Anson Hoyt <ahoyt@tiu11.org>
2022-04-07 14:14:01 -0400

    EmailValidator: tighten regex so clearly invalid domains are rejected. Use newer errors syntax.

--- rails/app/validators/email_validator.rb
+++ rails/app/validators/email_validator.rb
@@ -1,12 +1,37 @@
 # frozen_string_literal: true
 
-# TODO: Consider improving with:
-# * http://my.rails-royce.org/2010/07/21/email-validation-in-ruby-on-rails-without-regexp/
-# * https://hackernoon.com/the-100-correct-way-to-validate-email-addresses-7c4818f24643#.cst8tslbp
+# Validates email format.
+# * Doesn't check for RFC-5321 compliance
+# * No dotless domains, prohibited per ICANN
+# * Allows internationalized domain name (IDN)
+#
+# Regex Examples:
+# * https://en.wikipedia.org/wiki/Email_address#Examples
+# * https://github.com/binarylogic/authlogic/blob/master/doc/use_normal_rails_validation.md
+# * https://github.com/heartcombo/devise/blob/main/lib/devise.rb#L116
+# * https://github.com/truemail-rb/truemail#with-default-regex-pattern
+#
+# Reference:
+# * SMTP specification
+#   https://datatracker.ietf.org/doc/html/RFC5321
+# * Technical specification for Top Level Domain Labels
+#   https://tools.ietf.org/id/draft-liman-tld-names-00.html
+# * New gTLD Dotless Domain Names Prohibited:
+#   https://www.icann.org/en/announcements/details/new-gtld-dotless-domain-names-prohibited-30-8-2013-en
 class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    return if value.match?(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i)
+  # /p{L} - letters in any language (for IDN support)
+  EMAIL = /
+    \A
+    [^@\s]+              # mailbox
+    @
+    (?:[\p{L}0-9\-]+\.)+ # subdomains (can be IDN)
+    [A-Z0-9\-]{2,63}     # TLD
+    \z
+  /ix.freeze
 
-    record.errors[attribute] << (options[:message] || "is not an email")
+  def validate_each(record, attribute, value)
+    return if value.match?(EMAIL)
+
+    record.errors.add attribute, (options[:message] || :invalid)
   end
 end
