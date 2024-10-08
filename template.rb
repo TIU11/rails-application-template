ad84050a61adb072685dc6c88dbae90890631b23
Anson Hoyt <ahoyt@tiu11.org>
2024-10-08 14:10:20 -0400

    Redundant with Rails 7.2 defaults

--- template.rb
+++ template.rb
@@ -26,7 +26,6 @@ insert_into_file 'config/environments/development.rb', <<-CONFIG, before: /^end/
 
   # Action Mailer
   config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 } # for mailcatcher
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.asset_host = "http://localhost:3000" # for image URLs in HTML email
 
   # Allow generating absolute urls with routing url helpers.
