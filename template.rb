5df7db04ce9edc3f1154ba3aa93d7c81451f4515
Anson Hoyt <ahoyt@tiu11.org>
2024-10-08 19:31:49 -0400

    Less words

--- template.rb
+++ template.rb
@@ -25,7 +25,7 @@ insert_into_file 'config/environments/development.rb', <<-CONFIG, before: /^end/
   config.action_controller.action_on_unpermitted_parameters = :raise
 
   # Action Mailer
-  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 } # for mailcatcher
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 } # mailcatcher
   config.action_mailer.asset_host = "http://localhost:3000" # for image URLs in HTML email
 
   # Allow generating absolute urls with routing url helpers.
